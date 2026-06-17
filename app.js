bcb4f5a0cb3acd263fa7d817bc18ac794901ae7b
poncjoaa <joaquinnicolasponce11@gmail.com>
2026-06-17 17:28:37 -0300

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+const supabaseUrl = "https://jyucjninnzwmycxtncjj.supabase.co";
+
+const supabaseKey =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5dWNqbmlubnp3bXljeHRuY2pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MTQ0NzMsImV4cCI6MjA5NzI5MDQ3M30.g8BiPO9wDPoWNk-nki0tRA4OLJn0fZuAqFskg3KEHBk";
+
+const supabaseClient = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
 const btnIngresar = document.getElementById("btnIngresar");
 
-btnIngresar.addEventListener("click", () => {
+btnIngresar.addEventListener("click", async () => {
 
     const username = document.getElementById("username").value;
+
     const password = document.getElementById("password").value;
 
-    if (username === "admin" && password === "123456") {
+    const { data, error } = await supabaseClient
+        .from("usuarios")
+        .select("*")
+        .eq("username", username)
+        .eq("password_hash", password)
+        .single();
+
+    if (data) {
 
         alert("Bienvenido a BarberOS");
 
